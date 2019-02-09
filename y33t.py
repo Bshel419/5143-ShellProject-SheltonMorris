1e94fd902fcf697e442848aeb38ce406da8cbb77
Benjamin Shelton <Bshel419@rocketmail.com>
2019-02-08 20:20:12 -0600

    REEE

--- y33t.py
+++ y33t.py
@@ -1,10 +1,46 @@
-'''
-import os
-
-path = '.'
-
-files = os.listdir(path)
-for file in files:
-    print(file)
-'''
 import y33ters
+
+
+if __name__ == "__main__":
+    '''
+    commandDic = { 
+        'cat': yt.cat,
+        'cd': yt.cd,
+        'cp': yt.cp,
+        'grep': yt.grep,
+        'head': yt.head,
+        'less': yt.less,
+        'ls': yt.ls,
+        'mkdir': yt.mkdir,
+        'mv': yt.mv,
+        'pwd': yt.pwd,
+        'rm': yt.rm,
+        'rmdir': yt.rmdir,
+        'sort': yt.sort,
+        'tail': yt.tail,
+        'wc': yt.wc,
+        'who': yt.who
+    }
+    '''
+
+    argsDic = {"Command": [], "Params": [], "Flags": [], "Output":[]}
+    history = []
+
+    loop = True
+
+    while(loop):
+        print(dir(y33ters))
+        cmd = input(">> ")
+        history.append(cmd)
+
+        cmd = cmd.split()
+        
+
+        print(cmd)
+
+        if cmd[0] == "cd":
+            y33ters.cd()
+
+        if cmd[0] == "REEE":
+            loop = False
+
