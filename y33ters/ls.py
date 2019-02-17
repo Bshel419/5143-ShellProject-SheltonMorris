1280c981f12ddf68fa350963b885f330a3afa2fa
garrett4311 <40669423+garrett4311@users.noreply.github.com>
2019-02-17 14:48:19 -0600

    ls now works with flags

--- y33ters/ls.py
+++ y33ters/ls.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from os import stat
 #from pwd import getpwuid
 #from grp import getgrgid
 from datetime import datetime
@@ -26,24 +27,46 @@ def ls(command, flags, params, output):
         for file in files:
             #isLink = os.path.islink(file)
             #isDir = os.path.isdir(file)
-            info = os.lstat(file)
-            #if not isLink and not isDir:
+            if not flags:
+                print(file)
+            else:
+                info = os.lstat(file)
+                #if not isLink and not isDir:
                 #isFile = True
-            octalPerms = oct(info.st_mode)[-3:]
-            octalPerms = int(octalPerms)
-            one = octalPerms % 10
-            octalPerms = octalPerms // 10
-            ten = octalPerms % 10
-            octalPerms = octalPerms // 10
-            print(permission[one] + permission[ten] + permission[octalPerms], end =" ")
-            print('@' + str(info.st_nlink), end =" ")
-            #print(getpwuid(os.stat(file).st_uid).pw_name)
-            #print(getgrgid(os.stat(file).st_gid).gr_name)
-            size = str(info.st_size).encode()
-            print(size, end =" ")
-            time = info.st_mtime
-            print(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'), end =" ")
-            print(file)
+                octalPerms = oct(info.st_mode)[-3:]
+                octalPerms = int(octalPerms)
+                one = octalPerms % 10
+                octalPerms = octalPerms // 10
+                ten = octalPerms % 10
+                octalPerms = octalPerms // 10
+                time = info.st_mtime
+                name = stat(file).st_uid
+                group = stat(file).st_gid
+                for f in flags:
+                    if f == '-l':
+                        print(permission[one] + permission[ten] + permission[octalPerms], end =" ")
+                        print('@' + str(info.st_nlink), end =" ")
+                        #print(name, end = " ")
+                        #print(group, end = " ")
+                        size = info.st_size
+                        print(size, end =" ")
+                        print(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'), end =" ")
+                        print(file)
+                    elif f == '-h':
+                        print(permission[one] + permission[ten] + permission[octalPerms], end =" ")
+                        print('@' + str(info.st_nlink), end =" ")
+                        #print(name, end = " ")
+                        #print(group, end = " ")
+                        size = info.st_size
+                        for unit in ['bytes', 'MB', 'KB', 'GB']:
+                            if size < 1024:
+                                hr_size = str(size) + unit
+                                break
+                            else:
+                                size /= 1024
+                        print(hr_size, end =" ")
+                        print(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'), end =" ")
+                        print(file)
     else:
         with open(output[0], 'w') as outfile:
             for file in files:
